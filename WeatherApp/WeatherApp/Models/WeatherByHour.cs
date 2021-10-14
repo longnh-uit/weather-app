209989c54f034f58c8bf65c14cdc44f40840a83f
longnh-uit <longnh.0601@gmail.com>
2021-10-14 17:21:12 +0700

    Implement classes

---
+++ WeatherApp/WeatherApp/Models/WeatherByHour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Models
+{
+    class WeatherByHour : IWeather
+    {
+        public double TempMin { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double TempMax { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double CurrentTemp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double WindSpeed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Humidity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Pressure { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime SunRise { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime SunSet { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime Date { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Visibility { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Icon { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
