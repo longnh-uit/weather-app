3ce481323ae3cd1b8b89c99b87c14339b5a2b207
longnh-uit <longnh.0601@gmail.com>
2021-11-07 15:05:16 +0700

    add flyout menu

---
+++ WeatherApp/WeatherApp/MainPageFlyout.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.View_Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public MainPageFlyout()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+            ListView = MenuItemsListView;
+        }
+    }
+}
--- WeatherApp/WeatherApp/View Models/MainPageViewModel.cs
+++ WeatherApp/WeatherApp/View Models/MainPageViewModel.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 
 namespace WeatherApp.View_Models
 {
-    class MainPageViewModel
+
+    class MainPageViewModel : INotifyPropertyChanged
     {
+        public ObservableCollection<MainPageFlyoutMenuItem> MenuItems { get; set; }
+
+        public MainPageViewModel()
+        {
+            MenuItems = new ObservableCollection<MainPageFlyoutMenuItem>(new[]
+            {
+                new MainPageFlyoutMenuItem { Id = 0, Title = "Thêm vị trí" },
+                new MainPageFlyoutMenuItem { Id = 1, Title = "Cài đặt đơn vị" },
+                new MainPageFlyoutMenuItem { Id = 2, Title = "Màu nền" }
+            });
+        }
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
