3921237ab71cd1f517227d472ef150701839b2d7
longnh-uit <longnh.0601@gmail.com>
2021-11-18 14:16:56 +0700

    pull Huy

--- WeatherApp/WeatherApp/Views/UnitSettingPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/UnitSettingPage.xaml.cs
@@ -12,9 +12,19 @@ namespace WeatherApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UnitSettingPage : ContentPage
     {
+        class test
+        {
+            public string Title { get; set; }
+        }
         public UnitSettingPage()
         {
             InitializeComponent();
+            List<test> list = new List<test>()
+            {
+                new test {Title = "°C"},
+                new test {Title = "°F"}
+            };
+            TemperaturePicker.ItemsSource = list;
         }
     }
 }
