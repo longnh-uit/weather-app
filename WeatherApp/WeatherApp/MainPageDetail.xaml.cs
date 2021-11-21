3191fb29af949524b18742a5491c94726e35e430
longnh-uit <longnh.0601@gmail.com>
2021-11-21 14:30:21 +0700

    realtime update main page

--- WeatherApp/WeatherApp/MainPageDetail.xaml.cs
+++ WeatherApp/WeatherApp/MainPageDetail.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,6 +17,11 @@ namespace WeatherApp
         {
             InitializeComponent();
             ItemsAdded();
+            Device.StartTimer(TimeSpan.FromSeconds(1), () => { Device.BeginInvokeOnMainThread(() => {
+                CultureInfo culture = CultureInfo.CreateSpecificCulture("vi-VN");
+                TimeLabel.Text = DateTime.Now.ToString("HH:mm");
+                DateLabel.Text = DateTime.Now.ToString("ddd", culture) + ", Th" + DateTime.Now.ToString("MM") + " " + DateTime.Now.ToString("dd");
+            }); return true; });
         }
         List<string> itemsList = new List<string>();
         void ItemsAdded()
