79f2db0e7aaf8224ce9d375234502a6c1a70cd57
huyvouit <huyvouit@gmail.com>
2021-11-21 13:06:23 +0700

    create database,test handle change bgColor

---
+++ WeatherApp/WeatherApp/Models/Variable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Models
+{
+     public class Variable
+    {
+        public int bgColorID { set; get; }
+        public string bgColorName { set; get; }
+        public string bgColorValue { set; get; }
+    }
+}
