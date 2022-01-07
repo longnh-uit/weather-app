892b922f7c4e08742bcded0056f03a764d99055a
huyvouit <huyvouit@gmail.com>
2022-01-08 00:12:03 +0700

    fix success validate

--- WeatherApp/WeatherApp/Views/ContactPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/ContactPage.xaml.cs
@@ -18,7 +18,7 @@ namespace WeatherApp.Views
         {
             InitializeComponent();
         }
-        
+
         public List<KeyValuePair<string, string>> error = new List<KeyValuePair<string, string>>()
         {
 
@@ -29,20 +29,67 @@ namespace WeatherApp.Views
             var emailPattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
             if (email.Length <= 0)
             {
-                error.Insert(0, new KeyValuePair<string, string>("email", "Nhập họ và tên"));              
+                var result = error.Where(item => item.Key == "email").ToList();
+                if (result.Count == 0)
+                {
+                    //error.Insert(0, new KeyValuePair<string, string>("email", "Nhập email"));
+                    error.Add(new KeyValuePair<string, string>("email", "Nhập email"));
+                }
+                else
+                {
+                    error.RemoveAll(x => x.Key.Equals("email"));
+                    //error.Insert(0, new KeyValuePair<string, string>("email", "Nhập email"));
+                    error.Add(new KeyValuePair<string, string>("email", "Nhập email"));
+                }
             }
             else if (!Regex.IsMatch(email, emailPattern))
             {
-                error.Insert(0, new KeyValuePair<string, string>("email", "Email không hợp lệ"));              
+                var result = error.Where(item => item.Key == "email").ToList();
+                if (result.Count == 0)
+                {
+                    error.Add(new KeyValuePair<string, string>("email", "Email không hợp lệ"));
+                    //error.Add(new KeyValuePair<string, string>("email", "Nhập email"));
+                }
+                else
+                {
+                    error.RemoveAll(x => x.Key.Equals("email"));
+                    error.Add(new KeyValuePair<string, string>("email", "Email không hợp lệ"));
+                }
+            }
+            else
+            {
+                error.RemoveAll(x => x.Key.Equals("email"));
             }
             if (name.Length <= 0)
             {
-                error.Insert(1, new KeyValuePair<string, string>("name", "Nhập họ và tên"));
-          
+                var result = error.Where(item => item.Key == "name").ToList();
+                if (result.Count == 0)
+                {
+                    //error.Insert(0, new KeyValuePair<string, string>("email", "Nhập email"));
+                    error.Add(new KeyValuePair<string, string>("name", "Nhập họ và tên"));
+                    //error.Add(new KeyValuePair<string, string>("email", "Nhập email"));
+                }
+
+            }
+            else
+            {
+                error.RemoveAll(x => x.Key.Equals("name"));
             }
             if (msg.Length <= 0)
             {
-                error.Insert(2, new KeyValuePair<string, string>("message", "Nhập nội dung"));        
+                var result = error.Where(item => item.Key == "message").ToList();
+                if (result.Count == 0)
+                {
+                    //error.Insert(0, new KeyValuePair<string, string>("email", "Nhập email"));
+                    //error.Insert(1, new KeyValuePair<string, string>("name", "Nhập họ và tên"));
+                    error.Add(new KeyValuePair<string, string>("message", "Nhập nội dung"));
+                    //error.Add(new KeyValuePair<string, string>("email", "Nhập email"));
+                }
+            }
+            else
+            {
+                error.RemoveAll(x => x.Key.Equals("message"));
+
             }
             if (error.Count > 0) return false;
             return true;
@@ -50,15 +97,45 @@ namespace WeatherApp.Views
         async void submmitBtn_Clicked(object sender, EventArgs e)
         {
             bool check = validate(emailTxt.Text, nameTxt.Text, msgTxt.Text);
-            if (!check)
+            if (check == false)
             {
-                errorEmail.Text = error[0].Value;
-                errorName.Text = error[1].Value;
-                errorMsg.Text = error[2].Value;
+                var resultEmail = error.Where(item => item.Key == "email").ToList();
+                var resultName = error.Where(item => item.Key == "name").ToList();
+                var resultMsg = error.Where(item => item.Key == "message").ToList();
+                if (resultEmail.Count == 1)
+                {
+                    var v = error.FirstOrDefault(x => x.Key == "email");
+                    errorEmail.Text = v.Value;
+                }
+                else
+                {
+                    errorEmail.Text = "";
+                }
+                if (resultName.Count == 1)
+                {
+                    var v = error.FirstOrDefault(x => x.Key == "name");
+                    errorName.Text = v.Value;
+                }
+                else
+                {
+                    errorName.Text = "";
+                }
+                if (resultMsg.Count == 1)
+                {
+                    var v = error.FirstOrDefault(x => x.Key == "message");
+                    errorMsg.Text = v.Value;
+                }
+                else
+                {
+                    errorMsg.Text = "";
+                }
                 return;
             }
             else
             {
+                errorEmail.Text = "";
+                errorName.Text = "";
+                errorMsg.Text = "";
                 Contact info = new Contact
                 {
                     email = emailTxt.Text,
