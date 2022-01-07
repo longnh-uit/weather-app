3967c3a336221a9b99f4ff67193c0cc75d2dc319
huyvouit <huyvouit@gmail.com>
2022-01-07 11:43:42 +0700

    add validate failed

--- WeatherApp/WeatherApp/Helper/ApiCaller.cs
+++ WeatherApp/WeatherApp/Helper/ApiCaller.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using WeatherApp.Models;
 namespace WeatherApp.Helper
 {
     public class ApiCaller
@@ -23,17 +24,10 @@ namespace WeatherApp.Helper
             }
         }
 
-        public static async Task<ApiResponse> PostContact(string email, string name, string msg)
+        public static async Task<ApiResponse> PostContact(Contact info)
         {
             using (var client = new HttpClient())
             {
-                var info = new
-                {
-                    email = email,
-                    name = name,
-                    message = msg,
-                };
-
                 string url = "https://chito-stationery.herokuapp.com/contact/add";
                 StringContent content = new StringContent(JsonConvert.SerializeObject(info), Encoding.UTF8, "application/json");
                 HttpResponseMessage response = await client.PostAsync(url, content);
--- WeatherApp/WeatherApp/Views/ContactPage.xaml.cs
+++ WeatherApp/WeatherApp/Views/ContactPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.RegularExpressions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using WeatherApp.Helper;
@@ -18,18 +18,66 @@ namespace WeatherApp.Views
         {
             InitializeComponent();
         }
+        
+        public List<KeyValuePair<string, string>> error = new List<KeyValuePair<string, string>>()
+        {
 
+        };
+        public bool validate(string email, string name, string msg)
+        {
+
+            var emailPattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+            if (email.Length <= 0)
+            {
+                error.Insert(0, new KeyValuePair<string, string>("email", "Nhập họ và tên"));              
+            }
+            else if (!Regex.IsMatch(email, emailPattern))
+            {
+                error.Insert(0, new KeyValuePair<string, string>("email", "Email không hợp lệ"));              
+            }
+            if (name.Length <= 0)
+            {
+                error.Insert(1, new KeyValuePair<string, string>("name", "Nhập họ và tên"));
+          
+            }
+            if (msg.Length <= 0)
+            {
+                error.Insert(2, new KeyValuePair<string, string>("message", "Nhập nội dung"));        
+            }
+            if (error.Count > 0) return false;
+            return true;
+        }
         async void submmitBtn_Clicked(object sender, EventArgs e)
         {
-            var result = await ApiCaller.PostContact(emailTxt.Text, nameTxt.Text, msgTxt.Text);
-            if (result.Successful)
+            bool check = validate(emailTxt.Text, nameTxt.Text, msgTxt.Text);
+            if (!check)
             {
-                Notify notify = JsonConvert.DeserializeObject<Notify>(result.Response);
-                await DisplayAlert("Thông báo", notify.msg, "OK");
+                errorEmail.Text = error[0].Value;
+                errorName.Text = error[1].Value;
+                errorMsg.Text = error[2].Value;
+                return;
             }
             else
             {
-                await DisplayAlert("Thông báo", "Đã có lỗi xảy ra. Vui lòng thử lại", "OK");
+                Contact info = new Contact
+                {
+                    email = emailTxt.Text,
+                    name = nameTxt.Text,
+                    message = msgTxt.Text,
+                };
+                var result = await ApiCaller.PostContact(info);
+                if (result.Successful)
+                {
+                    Notify notify = JsonConvert.DeserializeObject<Notify>(result.Response);
+                    await DisplayAlert("Thông báo", notify.msg, "OK");
+                    emailTxt.Text = "";
+                    nameTxt.Text = "";
+                    msgTxt.Text = "";
+                }
+                else
+                {
+                    await DisplayAlert("Thông báo", "Đã có lỗi xảy ra. Vui lòng thử lại", "OK");
+                }
             }
         }
     }
